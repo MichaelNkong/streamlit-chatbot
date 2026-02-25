342b8e1e28f72428cfec29f4e4044021bb0284d9
MichaelNkong <nkongho.ndip@gmail.com>
2026-02-25 14:25:11 +0100

    refactored code

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, AIMessage
+
 import os
+
+from logic.chathelper import chathelper
+
 load_dotenv()
 
 #streamlit page setup
@@ -64,81 +66,19 @@ model = st.selectbox(
 st.write(f"Provider: **{provider}**")
 st.write(f"Model: **{model}**")
 user_prompt = st.chat_input("Ask chatbot")
-if provider == "OpenAI":
-    llm = ChatOpenAI(
+if user_prompt:
+   if provider == "OpenAI":
+      llm = ChatOpenAI(
         model=model,
         api_key=os.getenv("OPENAI_API_KEY"),
         temperature=0
-    )
+      )
+      chathelper(llm,user_prompt)
 
-    if user_prompt:
-         # Show user message
-       st.chat_message("user").markdown(user_prompt)
-
-       # Save user message
-       st.session_state.chat_history.append(
-           {"role": "user", "content": user_prompt}
-        )
-
-       # Build prompt from full chat history
-       prompt = ChatPromptTemplate.from_messages(
-        st.session_state.chat_history
-        )
-
-        # Create chain
-       chain = prompt | llm
-
-       # Call model
-       response = chain.invoke({})
-
-        # Extract content
-       assistant_message = response.content
-
-       # Show assistant message
-       st.chat_message("assistant").markdown(assistant_message)
-
-    # Save assistant message
-       st.session_state.chat_history.append(
-          {"role": "assistant", "content": assistant_message}
-       )
-elif provider == "Gemini":
-    llm = ChatGoogleGenerativeAI(
-        model=model,
-        google_api_key=os.getenv("GEMINI_API_KEY"),
-        temperature=0
-    )
-    if user_prompt:
-        # Show user message
-        st.chat_message("user").markdown(user_prompt)
-
-        # Save user message
-        st.session_state.chat_history.append(
-            {"role": "user", "content": user_prompt}
-        )
-
-        # Convert history to LangChain messages
-        messages = []
-        for msg in st.session_state.chat_history:
-            if msg["role"] == "user":
-                messages.append(HumanMessage(content=msg["content"]))
-            else:
-                messages.append(AIMessage(content=msg["content"]))
-
-        # Build prompt
-        prompt = ChatPromptTemplate.from_messages(messages)
-
-        # Create chain
-        chain = prompt | llm
-
-        # Call model
-        response = chain.invoke({})
-
-        assistant_message = response.content
-
-        # Show assistant message
-        st.chat_message("assistant").markdown(assistant_message)
-
-        # Save assistant message
-        st.session_state.chat_history.append(
-            {"role": "assistant", "content": assistant_message}
-        )
+   elif provider == "Gemini":
+        llm = ChatGoogleGenerativeAI(
+          model=model,
+          google_api_key=os.getenv("GEMINI_API_KEY"),
+          temperature=0
+         )
+        chathelper(llm, user_prompt)
---
+++ logic/chathelper.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+def chathelper(llm,user_prompt):
+        # Show user message
+        st.chat_message("user").markdown(user_prompt)
+
+        # Save user message
+        st.session_state.chat_history.append(
+            {"role": "user", "content": user_prompt}
+        )
+
+        # Build prompt from full chat history
+        prompt = ChatPromptTemplate.from_messages(
+            st.session_state.chat_history
+        )
+
+        # Create chain
+        chain = prompt | llm
+
+        # Call model
+        response = chain.invoke({})
+
+        # Extract content
+        assistant_message = response.content
+
+        # Show assistant message
+        st.chat_message("assistant").markdown(assistant_message)
+
+        # Save assistant message
+        st.session_state.chat_history.append(
+            {"role": "assistant", "content": assistant_message}
+        )
